9a00cc9bf443c1731eb64d85a79560a76217c1dc
Initial config <douglas.w.estevao@gmail.com>
2025-07-29 22:33:50 -0300

    atualizando a bliblioteca dotenv para variaveis globais

--- test/login.test.js
+++ test/login.test.js
@@ -1,20 +1,23 @@
     const request = require('supertest');
     const {expect} = require('chai');
+    require('dotenv').config();
+
 
     describe('Login', ()=> {
         describe('POST/login', ()=>{
             it('Deve retornar 200 com um token em string quando usar credenciais validas',async ()=> {
-                    const resposta = await request('http://localhost:3000')
-                        .post('/login')
-                        .set('Content-Type', 'application/json')
-                        .send({
-                            'username': 'julio.lima',
-                             'senha':  '123456'
-                            })                          
-                    
 
-                    expect(resposta.status).to.equal(200);
-                    expect(resposta.body.token).to.be.a('string');                  
+                const respostaLogin = await request(process.env.BASE_URL)
+                    .post('/login')
+                    .set('Content-Type', 'application/json')
+                    .send({
+                        'username': 'julio.lima',
+                        'senha':  '123456'
+                     }); 
+
+                    
+                    expect(respostaLogin.status).to.equal(200);
+                    expect(respostaLogin.body.token).to.be.a('string');                  
                     
 
         
--- test/tranferencia.test.js
+++ test/tranferencia.test.js
@@ -1,54 +1,64 @@
 const request = require('supertest');
 const {expect} = require('chai');
+require('dotenv').config();
+
+
 
 describe('Transferencias', () => {
-    describe('POST/transferencias',()=> {
-        it('Deve retornar sucesso com 201 quando o valor for igual ou acima de 10,00',async () =>{
+  describe('POST /transferencias', () => {
 
-            const respostaLogin = await request('http://localhost:3000')
-                        .post('/login')
-                        .set('Content-Type', 'application/json')
-                        .send({
-                            'username': 'julio.lima',
-                            'senha':  '123456'
-                        })                        
-                        
-                        const token = respostaLogin.body.token
-            
-            
-            const resposta = await request('http://localhost:3000')
-                        .post('/,transferencias')
-                        .set('Content-Type', 'application/json')
-                        .set ('Authorization', `Bearer ${token}`)
-                        .send( {
-                                'contaOrigem': 1,
-                                'contaDestino': 2,
-                                'valor': 11,
-                                'token': ''
-                            })
+    it('Deve retornar sucesso com 201 quando o valor for igual ou acima de 10,00', async () => {
 
-                                 expect(resposta.status).to.equal(201);
-                                
-                                
+      const respostaLogin = await request(process.env.BASE_URL)
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'julio.lima',
+          senha: '123456'
+        });
 
-    })
+      const token = respostaLogin.body.token;
 
+      const resposta = await request(process.env.BASE_URL)
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 11,
+          token: ''
+        });
 
-        it('Deve retornar erro com 422 quando o valor da transferencia for abaixo de 10,00', async() =>{
-            const resposta = await request('http://localhost:3000')
-                        .post('/,transferencias')
-                        .set('Content-Type', 'application/json')
-                        .set ('Authorization', `Bearer ${token}`)
-                        .send( {
-                                'contaOrigem': 1,
-                                'contaDestino': 2,
-                                'valor': 7,
-                                'token': ''
-                            })
+      
+      expect(resposta.status).to.equal(201);
+    });
 
-                                 expect(resposta.status).to.equal(422)
+    it('Deve retornar erro com 422 quando o valor da transferência for abaixo de 10,00', async () => {
 
-        })
-    })
+      const respostaLogin = await request(process.env.BASE_URL)
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'julio.lima',
+          senha: '123456'
+        });
 
-})
+      const token = respostaLogin.body.token;
+
+      const resposta = await request(process.env.BASE_URL)
+        .post('/transferencias')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          contaOrigem: 1,
+          contaDestino: 2,
+          valor: 7,
+          token: ''
+        });
+
+      expect(resposta.status).to.equal(422);
+    });
+
+  });
+});
